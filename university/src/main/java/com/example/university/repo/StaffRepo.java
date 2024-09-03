837cdca3d4549e2e81d0c23ec115fe10e5bcb1cf
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-09-03 19:49:04 +0000

    add JpaRepository

---
+++ university/src/main/java/com/example/university/repo/StaffRepo.java
@@ -0,0 +1,8 @@
+package com.example.university.repo;
+
+import com.example.university.domain.Staff;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface StaffRepo extends JpaRepository<Staff, Integer> {
+
+}
