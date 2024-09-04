c1c959f1ccd3508b01ca6885146a0b75fbbfa345
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-09-04 14:06:11 +0000

    query by one attribute

--- university/src/main/java/com/example/university/dao/StudentDao.java
+++ university/src/main/java/com/example/university/dao/StudentDao.java
@@ -20,24 +20,6 @@ public class StudentDao {
         this.em = emf.createEntityManager();
     }
     
-    List<Student> findByFullTime(boolean fullTime){
-        TypedQuery<Student> query = em.createQuery(
-                "SELECT s FROM Student s WHERE s.fullTime = :fullTime" , Student.class);
-        return query.setParameter("fullTime", fullTime).getResultList();
-    }
-
-    List<Student> findByAge(Integer age) {
-        TypedQuery<Student> query = em.createQuery(
-                "SELECT s FROM Student s WHERE s.age = :age" , Student.class);
-        return query.setParameter("age", age).getResultList();
-    }
-
-    List<Student> findByLastName(String lastName){
-        TypedQuery<Student> query = em.createQuery(
-                "SELECT s FROM Student s WHERE s.attendee.lastName = :lastName" , Student.class);
-        return query.setParameter("lastName", lastName).getResultList();
-    }
-
 
     Optional<Student> findOldest() {
         TypedQuery<Student> query = em.createQuery(
--- university/src/main/java/com/example/university/repo/CourseRepo.java
+++ university/src/main/java/com/example/university/repo/CourseRepo.java
@@ -1,8 +1,18 @@
 package com.example.university.repo;
 
 import com.example.university.domain.Course;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface CourseRepo extends JpaRepository<Course, Integer>{
+
+  Optional<Course> findByName(String name);
+  
+  List<Course> findByPrerequisites(Course prerequisite);
+
+  List<Course> findByCredits(int credits);
+
+  List<Course> findByDepartmentChairMemberLastName(String chair);
 }
