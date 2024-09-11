720c17d7377c511826d930d86cb32b5540f7ab8e
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-09-11 01:36:58 +0000

    query by example challenge

--- university/src/test/java/com/example/university/dao/CriteriaQueryTest.java
+++ university/src/test/java/com/example/university/dao/CriteriaQueryTest.java
@@ -1,21 +1,23 @@
 package com.example.university.dao;
 
 import static com.example.university.business.CourseFilter.filterBy;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
 
 import com.example.university.business.CourseFilter;
 import com.example.university.business.DynamicQueryService;
 import com.example.university.business.UniversityService;
 import com.example.university.domain.Department;
+import com.example.university.domain.Person;
 import com.example.university.domain.Staff;
 import com.example.university.repo.DepartmentRepo;
 import com.example.university.repo.StaffRepo;
 
+import static org.springframework.data.domain.ExampleMatcher.matching;
 /**
  * Test Criteria-based queries
  */
@@ -34,8 +36,14 @@ public class CriteriaQueryTest {
     @Test
     void findByCriteria() {
         UniversityFactory.fillUniversity(universityService);
-        Department humanities = departmentRepo.findByName("Humanities").get();
-        Staff professorBlack = staffRepo.findByLastName("Black")
+
+        Department humanities = departmentRepo.findOne(
+            Example.of(new Department("Humanities",null), 
+                matching().withIgnoreCase())).get();
+
+        Staff professorBlack = staffRepo.findAll(
+            Example.of(new Staff(new Person(null,"Black")),
+                matching().withIgnoreCase()))
                 .stream().findFirst().get();
 
         System.out.println('\n' + "*** All Humanities Courses");
