8426a04c11eac78e82c1abe2324a8910eb9cd97f
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-09-19 01:56:18 +0000

    simplify

--- college/src/main/java/com/example/college/CollegeApplication.java
+++ college/src/main/java/com/example/college/CollegeApplication.java
@@ -1,13 +1,12 @@
 package com.example.college;
 
-import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.college.domain.Department;
@@ -35,20 +34,19 @@ public class CollegeApplication implements CommandLineRunner{
 
     @Override
     public void run(String... args) {
-        int staffCount = staffRepo.count().block();
-        Mono<Staff> deanJonesMono = staffRepo.save(new Staff(new Person("John", "Jones")));
-        Mono<Staff> deanMartinMono = staffRepo.save(new Staff(new Person("John", "Martin")));
-        staffCount = staffRepo.count().block();
-        Staff deanJones = deanJonesMono.block();
-        Staff deanMartin = deanMartinMono.block();
-        staffCount = staffRepo.count().block();
+    
+        Staff deanJones = staffRepo
+        .save(new Staff(new Person("John", "Jones")))
+        .block();
+        Staff deanMartin = staffRepo
+        .save(new Staff(new Person("John", "Martin")))
+        .block();
         
-        Flux<Department> departmentFlux = departmentRepo.saveAll(
-                List.of(new Department("Humanities", deanJones),
-                        new Department("Natural Sciences", deanMartin),
-                        new Department("Social Sciences", deanJones)));
-        departmentFlux.subscribe(dept -> 
-            System.out.println(dept.getName() + " Saved"));
+        departmentRepo.saveAll(
+            List.of(new Department("Humanities", deanJones),
+                    new Department("Natural Sciences", deanMartin),
+                    new Department("Social Sciences", deanJones)))
+            .subscribe();
     }
 
     @GetMapping("/staff")
@@ -56,23 +54,8 @@ public class CollegeApplication implements CommandLineRunner{
         return staffRepo.findAll();
     }
 
-    @GetMapping("/staff/{id}")
-    public Mono<Staff> getStaffById(@PathVariable("id") String id) {
-        return staffRepo.findById(id);
-    }
-
     @GetMapping("/departments")
     public Flux<Department> getAllDepartments() {
         return departmentRepo.findAll();
-    }
-
-    @GetMapping("/departments/{id}")
-    public Mono<Department> getDepartmentById(@PathVariable("id") String id) {
-        return departmentRepo.findById(id);
-    }
-
-    @GetMapping("/staff/search/member/{lastName}")
-    public Flux<Staff> getStaffByLastName(@PathVariable("lastName") String lastName) {
-        return staffRepo.findByMemberLastName(lastName);
     }
 }
