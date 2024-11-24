754cbeb06bf9e84364687a9aae901505d074a5a9
Mary Ellen Bowman <maryellen.bowman@gmail.com>
2024-11-24 21:00:36 +0000

    update pre

--- university/src/main/java/com/example/university/dao/CourseDao.java
+++ university/src/main/java/com/example/university/dao/CourseDao.java
@@ -65,12 +65,12 @@ public class CourseDao {
         return query.setParameter("chair", chair).getResultList();
     }
 
-    public List<Course> findCourseByPrerequisite(int id) {
+    public List<Course> findByPrerequisites(Course course) {
         TypedQuery<Course> query = em.createQuery(
                 "Select c from Course c join c.prerequisites p where p.id = ?1", Course.class);
-        return query.setParameter(1, id).getResultList();
+        return query.setParameter(1, course.getId()).getResultList();
     }
-
+    
     public List<Course> findByCredits(int credits) {
         TypedQuery<Course> query = em.createQuery(
                 "SELECT c FROM Course c WHERE c.credits = :credits", Course.class);
